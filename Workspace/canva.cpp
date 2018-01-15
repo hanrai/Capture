bf9d83af88f104adbeec9dab8af54a805755267f
hanrai <hanrai@gmail.com>
2018-01-15 13:44:35 +0800

    Add Draging logic

--- Workspace/canva.cpp
+++ Workspace/canva.cpp
@@ -2,6 +2,7 @@
 #include <QPainter>
 #include <QPaintEvent>
 #include <QGraphicsPixmapItem>
+#include <QScrollBar>
 #include "canva.h"
 #include "CanvaMachine.h"
 
@@ -10,7 +11,8 @@ Canva::Canva(QWidget *parent) :
     m_machine(new CanvaMachine(this)),
     m_scale(1),
     m_imageItem(nullptr),
-    m_scene(new QGraphicsScene)
+    m_scene(new QGraphicsScene),
+    m_imageLoaded(false)
 {
     setScene(m_scene);
     setRenderHint(QPainter::Antialiasing);
@@ -19,6 +21,7 @@ Canva::Canva(QWidget *parent) :
     setViewportUpdateMode(QGraphicsView::SmartViewportUpdate);
     setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
     setMouseTracking(true);
+    setupMachine();
 
     m_machine->start();
     resetBackgroundPattern();
@@ -40,14 +43,21 @@ void Canva::setImage(QImage image)
 
     m_image = image;
 
-    if(m_imageItem == nullptr)
-    {
-        m_imageItem = new QGraphicsPixmapItem;
-        m_imageItem->setTransformationMode(Qt::SmoothTransformation);
-        m_scene->addItem(m_imageItem);
+    if(image.isNull()) {
+        Q_ASSERT(m_imageItem);
+        m_scene->removeItem(m_imageItem);
+        delete m_imageItem;
+        m_imageItem = nullptr;
+        setImageLoaded(false);
+    } else {
+        if(m_imageItem == nullptr) {
+            m_imageItem = new QGraphicsPixmapItem;
+            m_imageItem->setTransformationMode(Qt::SmoothTransformation);
+            m_scene->addItem(m_imageItem);
+        }
+        m_imageItem->setPixmap(QPixmap::fromImage(m_image));
+        setImageLoaded(true);
     }
-
-    m_imageItem->setPixmap(QPixmap::fromImage(m_image));
 
     m_machine->submitEvent("ImageChanged");
     emit imageChanged(m_image);
@@ -58,20 +68,23 @@ void Canva::setImage(QImage image)
 
 //}
 
-//void Canva::mouseMoveEvent(QMouseEvent *event)
-//{
+void Canva::mouseMoveEvent(QMouseEvent *event)
+{
+    submitMouseEvent("Mouse.Moved", event);
+    QGraphicsView::mouseMoveEvent(event);
+}
 
-//}
+void Canva::mousePressEvent(QMouseEvent *event)
+{
+    submitMouseEvent("Mouse.Pressed", event);
+    QGraphicsView::mousePressEvent(event);
+}
 
-//void Canva::mousePressEvent(QMouseEvent *event)
-//{
-
-//}
-
-//void Canva::mouseReleaseEvent(QMouseEvent *event)
-//{
-
-//}
+void Canva::mouseReleaseEvent(QMouseEvent *event)
+{
+    submitMouseEvent("Mouse.Released", event);
+    QGraphicsView::mouseReleaseEvent(event);
+}
 
 //void Canva::wheelEvent(QWheelEvent *event)
 //{
@@ -110,4 +123,35 @@ void Canva::resetBackgroundPattern()
     setPalette(palette);
 
     update();
+}
+
+void Canva::setupMachine()
+{
+    connect(this, &Canva::imageLoadedChanged, [this](const bool loaded){
+        m_machine->submitEvent(loaded ? "ImageLoaded" : "ImageUnloaded");
+    });
+
+    m_machine->connectToState("Movable.Set.Move", this,
+        [this](const bool isDraging) {
+        setCursor(isDraging ? Qt::ClosedHandCursor : Qt::ArrowCursor);
+    });
+
+    m_machine->connectToEvent("Draging", this,
+        [this](const QScxmlEvent &event) {
+        auto dx = event.data().toMap().value("x").toInt();
+        auto dy = event.data().toMap().value("y").toInt();
+        auto *scrollBar = horizontalScrollBar();
+        scrollBar->setValue(scrollBar->value() - dx);
+        scrollBar = verticalScrollBar();
+        scrollBar->setValue(scrollBar->value() - dy);
+    });
+}
+
+void Canva::submitMouseEvent(QString name, QMouseEvent *event) const
+{
+    m_machine->submitEvent(name, QVariantMap({
+            { "x", event->pos().x() },
+            { "y", event->pos().y() },
+            { "button", event->button()}
+        }));
 }
--- Workspace/canva.h
+++ Workspace/canva.h
@@ -3,6 +3,7 @@
 
 #include <QGraphicsView>
 #include <QImage>
+#include <QtDebug>
 
 class CanvaMachine;
 class QGraphicsScene;
@@ -11,6 +12,7 @@ class Canva : public QGraphicsView
 {
     Q_OBJECT
     Q_PROPERTY(QImage image READ image WRITE setImage NOTIFY imageChanged)
+    Q_PROPERTY(bool imageLoaded READ imageLoaded NOTIFY imageLoadedChanged)
 public:
     explicit Canva(QWidget *parent = nullptr);
     ~Canva();
@@ -20,30 +22,47 @@ public:
         return m_image;
     }
 
+    bool imageLoaded() const
+    {
+        return m_imageLoaded;
+    }
+
 signals:
     void imageChanged(QImage image);
+    void imageLoadedChanged(bool imageLoaded);
 
 public slots:
     void setImage(QImage image);
 
+private slots:
+    void setImageLoaded(bool imageLoaded)
+    {
+        if (m_imageLoaded == imageLoaded)
+            return;
+        m_imageLoaded = imageLoaded;
+        emit imageLoadedChanged(m_imageLoaded);
+    }
+
 protected:
 //    void mouseDoubleClickEvent(QMouseEvent *event) override;
-//    void mouseMoveEvent(QMouseEvent *event) override;
-//    void mousePressEvent(QMouseEvent *event) override;
-//    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
 //    void wheelEvent(QWheelEvent *event) override;
 //    void resizeEvent(QResizeEvent *event) override;
 
 private:
     void resetBackgroundPattern();
+    void setupMachine();
+    void submitMouseEvent(QString name, QMouseEvent *event) const;
 
 private:
     CanvaMachine *m_machine;
     QImage m_image;
     qreal m_scale;
-    QRect m_sourceRect;
     QGraphicsPixmapItem *m_imageItem;
     QGraphicsScene *m_scene;
+    bool m_imageLoaded;
 };
 
 #endif // CANVA_H
